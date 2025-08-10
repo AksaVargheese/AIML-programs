48371d7b775dad652e7e28aa2feb72504da7c26f
Aksa Vargheese <akskv20205210@gmail.com>
2025-08-10 16:22:11 +0530

    completed

--- data_height.py
+++ data_height.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as pt
 import sklearn.linear_model as lm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,mean_squared_error
+import math
 mydata=pd.read_csv("data_height.csv")
 x=mydata[["height"]]
 y=mydata[["weight"]]
-pt.scatter(x,y)
-pt.show()
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+pt.scatter(x_train,y_train)
+pt.show
 model=lm.LinearRegression()
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)
 model.fit(x,y)
 print(model.predict([[160]]))
